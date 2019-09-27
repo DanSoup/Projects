336c327e29ee1559c1c4762b422c0c39c8ed732a
DanSoup <dansoup@hotmail.co.uk>
2019-09-27 11:50:16 +0100

    added tests and testing

---
+++ javascript/fizzbuzz/tests.js
@@ -0,0 +1,8 @@
+const tests = [
+  {arg: 1, expected: 1},
+  {arg: 2, expected: 2},
+  {arg: 3, expected: 'Fizz'},
+  {arg: 4, expected: 4},
+  {arg: 5, expected: 'Buzz'},
+  {arg: 15, expected: 'FizzBuzz'},
+]
